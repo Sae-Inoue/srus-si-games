1a9bce9debd8aa0f406018672e185569fd2b98e9
Sae Inoue <20103543@tafe.wa.edu.au>
2024-07-30 14:26:35 +0800

    step4:create the Player class

--- app/player_node.py
+++ app/player_node.py
@@ -1,5 +1,38 @@
+from player import Player
 class PlayerNode():
-    def __init__(self, player, node, next_node) -> None:
-        self.__player = player
-        self.__node = node
-        self.__next_node = next_node
+    def __init__(self, player: Player) -> None:
+        self._player = player
+        self._previous = None
+        self._next = None
+
+
+    @property
+    def player(self) -> Player:
+        return self._player
+
+    # This sets the property and setter for previous
+    @property
+    def previous(self) -> Player:
+        return self._previous
+
+    # This sets the property and setter for next
+    @property
+    def next(self) -> Player:
+        return self._next
+
+    @previous.setter
+    def previous(self, previous_node: Player):
+        self._previous = previous_node
+
+    @next.setter
+    def next(self, next_node: Player):
+        self._next = next_node
+
+# Creating a new variable called key
+    @property
+    def key(self):
+        return self._player.uid
+
+    def __str__(self) -> str:
+        return f"PlayerNode(Player:{self._player}, Previous={self._previous}, Next={self._next})"
+

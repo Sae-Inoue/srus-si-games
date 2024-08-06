df697392d51a318b06dbd9313415a76b9af45718
Sae Inoue <20103543@tafe.wa.edu.au>
2024-08-06 15:06:51 +0800

    step5 b and c still working

--- app/player_list.py
+++ app/player_list.py
@@ -14,9 +14,33 @@ class PlayerList():
 
         if self.is_empty():
             self._head = new_node
+            print("This is empty")
         else:
             new_node.next = self._head
             new_node.previous = new_node
             self._head = new_node
+            print("this is not empty")
 
+    def __str__(self):
+        node = []
+        current = self._head
+        node.append(str(current))
+
+
+def main():
+    player = Player("1", "Sae")
+    #player2 = Player("2", "John")
+    plnode = PlayerNode(player)
+    pl1 = PlayerList()
+    list = pl1.insert_head(player)
+    #list = list.insert_head(player2)
+
+    print(str(player))
+    print(str(plnode))
+    print(list)
+    #print(list2)
+
+
+if __name__ == '__main__':
+    main()
 
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -8,12 +8,18 @@ class TestPlayer(unittest.TestCase):
         # Create an instance of playerList class
         self.playerlist = PlayerList()
         self.player = Player(uid="1", name="Sae")
+        self.player2 = Player(uid="2", name="John")
 
     def test_is_empty(self):
         self.assertTrue(self.playerlist.is_empty)
 
-    #def test_insert_head(self):
-        #self.assertEqual(self.playerlist.insert_head(self.player), )
+    def test_insert_head(self):
+        self.playerlist.insert_head(self.player)
+        self.playerlist.insert_head(self.player2)
+        self.assertEqual(self.playerlist, )
+
+    def test_str(self):
+        self.assertEqual(str(self.playerlist.__str__()), "Player id:1 , Player name:Sae")
 
 if __name__ == '__main__':
     unittest.main()

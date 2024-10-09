aec1e67d307c0fafbf32cbc1cf5658a87940b626
Sae Inoue <flower.smile120717@gmail.com>
2024-10-09 17:48:36 +0800

    feat: Optimise Binary Search Tree

--- app/player_bst.py
+++ app/player_bst.py
@@ -25,6 +25,7 @@ class PlayerBST():
         new_node.value = player
 
         if self._root is None:
+            print(f"This is root player: {new_node.key}")
             self._root = new_node
 
         else:
@@ -71,6 +72,58 @@ class PlayerBST():
         print(f"{name} is found!!")
         return current.player
 
+    def traverse(self):
+        '''
+        Traverse the current node and return its children
+        :return:
+        '''
+        nodes = []
+        print("In-order traversal:")
+        self.inorder_helper(self._root, nodes)
+        return nodes
+
+    def inorder_helper(self, node, nodes):
+        '''
+        Helper function to traverse the tree in order and return a list of PlayerBNodes
+        :param node:
+        :param nodes:
+        :return:
+        '''
+        if node is not None:
+            self.inorder_helper(node.left, nodes)
+            nodes.append(node)
+            print(str(node.player.name))
+            self.inorder_helper(node.right, nodes)
+
+    def balanced_tree(self, nodes, start, end):
+        '''
+        Balanced tree
+        :param nodes: a list of PlayerBNodes
+        :param start: at the start of the tree
+        :param end: at the end of the tree
+        :return:
+        '''
+        if start > end:
+            return None
+
+        mid = (start + end) // 2
+        self._root = PlayerBNode(nodes[mid])
+
+        self._root.left = self.balanced_tree(nodes, start, mid - 1)
+        self._root.right = self.balanced_tree(nodes, mid + 1, end)
+        return self._root
+
+    def balance_bst(self):
+        '''
+        Balanced tree traversal using binary search tree
+        :return: balanced tree function
+        '''
+        nodes = []
+        self.inorder_helper(self._root, nodes)
+        return self.balanced_tree(nodes, 0, len(nodes) - 1)
+
+
+
 
 def main():
     tree = PlayerBST()
@@ -79,16 +132,27 @@ def main():
     player2 = Player("2", "John", 15)
     player3 = Player("3", "Alice", 49)
     player4 = Player("4", "Levi", 34)
+    player5 = Player("5", "Josh", 34)
+    player6 = Player("6", "Karen", 34)
+    player7 = Player("7", "Ben ", 34)
 
 
     tree.insert(player)
     tree.insert(player2)
     tree.insert(player3)
     tree.insert(player4)
-
+    tree.insert(player5)
+    tree.insert(player6)
+    tree.insert(player7)
 
     tree.search("Alice")
     tree.search("Karen")
+
+    tree.traverse()
+
+    print("\n")
+    print("This is balanced tree:")
+    tree.balance_bst()
 
 
 
